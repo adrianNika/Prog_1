f75f4bc196e72fe2cc12eb5bf542784bf24ac4bd
adrianNika <142963260+adrianNika@users.noreply.github.com>
2024-06-10 16:30:09 +0200

    save

--- code/web-chat/script_login.js
+++ code/web-chat/script_login.js
@@ -2,15 +2,45 @@
 let toggleButton = document.getElementById('login-toggle');
 let classLogin = document.querySelector('.login');
 let classInscription = document.querySelector('.inscription');
+let submitInput = document.getElementById('submit-input');
+let msgAccount = document.getElementById('msg-account');
+
+let buttonVisibleMdp = document.getElementById('btnVisible');
+let iconMdp = document.querySelector('.bp-visible i');
+let inputPassword1 = document.getElementById('input-type1');
+let inputPassword2 = document.getElementById('input-type2');
 
     toggleButton.addEventListener('click', () => {
-        if (getComputedStyle(classLogin).display == 'none' || classLogin.style.display == 'none') {
-            toggleButton.innerText = 'Inscrire';
+        if (getComputedStyle(classLogin).display == 'none' || classLogin.style.display == 'none') 
+        {
+            toggleButton.innerText = 'S\'inscrire';
+            submitInput.innerText = 'Se connecter';
+            msgAccount.innerText = 'Vous n\'avez pas de compte ?';
             classInscription.style.display = 'none';
             classLogin.style.display = 'flex';
-        } else {
-            toggleButton.innerText = 'Login';
+        } 
+        else
+        {
+            toggleButton.innerText = 'Se connecter';
+            submitInput.innerText = 'S\'inscrire';
+            msgAccount.innerText = 'Vous avez déjà un compte ?';
             classLogin.style.display = 'none';
             classInscription.style.display = 'flex';
+        }
+    });
+
+
+    buttonVisibleMdp.addEventListener('click', () => {
+        if  (iconMdp.className == 'fa-solid fa-eye-slash')
+        {
+            iconMdp.className = 'fa-solid fa-eye';
+            inputPassword1.type = 'password';
+            inputPassword2.type = 'password';
+        }
+        else
+        {
+            iconMdp.className = 'fa-solid fa-eye-slash';
+            inputPassword1.type = 'text';
+            inputPassword2.type = 'password';
         }
     });
